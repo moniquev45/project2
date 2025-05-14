2a2ebe8515a7398068699ebe44b2780b2467d9d2
105904848 <105904848@student.swin.edu.au>
2025-05-14 11:52:44 +1000

    manager page structure inputted

---
+++ manage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <!--Charset Declaration-->
+        <meta charset="utf-8">
+        <!--Page Description for File-->
+        <meta name="description" content="">
+        <!--Key Words for File-->
+        <meta name="keywords" content="">
+        <!--Author Information-->
+        <meta name="author" content="">
+
+        <title>Manager Page</title>
+        <!--Icon for the browser-->
+        <link rel="icon" href="images/Pear_Logo_Backgroundless.png" type="image/png">
+        <!-- Logo Link: https://www.shutterstock.com/image-vector/green-pear-vector-icon-logo-design-2308212369 -->
+
+        <link rel="stylesheet" href="styles/styles.css">
+        
+    </head>
+    <body>
+        <!-- Including the navigation links (nav.inc) file -->
+        <?php include 'nav.inc'; ?>
+
+        <!-- Inserting the Header -->
+        <?php include 'header.inc'; ?>
+
+        <main>
+
+        </main>
+        <!-- Including nav_footer.inc file -->
+        <?php include 'nav_footer.inc'; ?>
+
+        <!-- Including footer file -->
+        <?php include 'footer.inc'; ?>
+        
+    </body>
+</html>
