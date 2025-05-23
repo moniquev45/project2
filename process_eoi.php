5a49c12fcac8e119bb49e092a3c41e5c505b8371
105904848 <105904848@student.swin.edu.au>
2025-05-23 13:09:52 +1000

    receipt_eoi page set up to divide the process_eoi page into a processing pg and a display receipt

--- process_eoi.php
+++ process_eoi.php
@@ -220,6 +220,8 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
                         $query = "SELECT eoi_number, submission_time FROM eoi WHERE eoi_number = $last_id";
                         $result_table = mysqli_query($dbconn, $query);
                         $row = mysqli_fetch_assoc($result_table);
+                        $submission_time = $row['submission_time'];
+                        $formatted_time = date("h:i a d-m-Y", strtotime($submission_time));
 
                 // 7: echoing all the data proivided in the form in a confirmation notice
                         // Submission Receipt division
@@ -233,7 +235,7 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
                             // The Application Form EOI record
                             echo "<p><strong>Application Receipt:</strong> ".htmlspecialchars($row['eoi_number'])."</p>";
                             // The Timestamp
-                            echo "<p><strong>The time submitted was:</strong> ".htmlspecialchars($row['submission_time'])."</p>";
+                            echo "<p><strong>The time submitted was:</strong> ".htmlspecialchars($formatted_time)."</p>";
                             
                             // The form answers:
                             echo "<p><em>Please find the confirmation of details entered below.</em></p>";
